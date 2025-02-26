0637ac1c137bcf179bcb0b5cbf8b94b4648d04ad
Tewdric <manoelvictor756@gmail.com>
2025-02-25 23:51:19 -0300

    a

--- Login/main.py
+++ Login/main.py
@@ -1,78 +1,27 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QTableWidgetItem, QTableWidget
-from tela_inicial import Ui_MainWindow
+from tela_login import Ui_Form
 from PyQt5 import QtWidgets, uic
 
-class tela_inicial(QMainWindow):
+class tela_login(QtWidgets.QMainWindow, Ui_Form):
     def __init__(self):
-        super(tela_inicial, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.tela_cadastro = uic.loadUi("tela_cadastro.ui")  # Carrega a segunda tela
-
-        self.ui.pushButton_cadastro.clicked.connect(self.mostrar_telaCadastro)
-        self.ui.pushButton_comprar.clicked.connect(self.mostrar_telaCompra)
+        super().__init__()
+        self.setupUi(self)
+        self.cadastro = uic.loadUi("tela_cadastro.ui")
         
-
+        self.ui.pushButton_entrar.clicked.connect(self.mostrar_telaCadastro)
+    
     def mostrar_telaCadastro(self):
-        self.tela_cadastro.show()  # Mostra a segunda tela
-        self.hide()  # Oculta a primeira tela
+        self.cadastro.show()
+        self.close()
         
-        self.tela_cadastro.pushButton_home.clicked.connect(self.voltar_tela_inicial)
-        self.tela_cadastro.pushButton_cadastrarProduto.clicked.connect(self.cadastrando_produto)
-    
-    def mostrar_telaCompra(self):
-        self.tela_compra.show()  # Mostra a segunda tela
-        self.hide()  # Oculta a primeira tela
-        
-        self.tela_compra.pushButton_voltar.clicked.connect(self.voltar_tela_inicial)
-        self.tela_compra.pushButton_selecionar.clicked.connect(self.selecionar_produto)
-        self.tela_compra.pushButton_FinalizarCompra.clicked.connect(self.FinalizarCompra)
-    
-    def selecionar_produto(self):
-        self.tela_compra.tableWidget_2.setRowCount(self.tela_compra.tableWidget_2.rowCount()+1)#linha
-        self.tela_compra.tableWidget_2.setItem(self.tela_compra.tableWidget_2.rowCount()-1, 0, QTableWidgetItem(self.tela_compra.tableWidget.item(self.tela_compra.tableWidget.currentRow(), 0).text()))#coluna 0 = nome
-        self.tela_compra.tableWidget_2.setItem(self.tela_compra.tableWidget_2.rowCount()-1, 1, QTableWidgetItem(self.tela_compra.tableWidget.item(self.tela_compra.tableWidget.currentRow(), 1).text()))#coluna 1 = preço
-        self.tela_compra.tableWidget_2.setItem(self.tela_compra.tableWidget_2.rowCount()-1, 2, QTableWidgetItem("1"))#coluna 2 = quantidade
 
-     
-
-    def FinalizarCompra(self):
-        total = 0
-        for i in range(self.tela_compra.tableWidget_2.rowCount()):
-            total += float(self.tela_compra.tableWidget_2.item(i, 1).text())
-            quantidade = int(self.tela_compra.tableWidget_2.item(i, 2).text())
-            resultado = total * quantidade
-            
-            
-            
-        QMessageBox.about(self, "Total da compra", f"O total da compra é de R${resultado:.2f}")
-        
-    def voltar_tela_inicial(self):
-        self.tela_cadastro.hide()  # Oculta a segunda tela
-        self.tela_compra.hide()  # Oculta a terceira tela
-        self.show()  # Mostra a primeira tela
-        
-    def cadastrando_produto(self):
-        nome_produto = self.tela_cadastro.lineEdit.text()
-        valor_produto = self.tela_cadastro.lineEdit_2.text()
-        
-        self.tela_cadastro.tableWidget.setRowCount(self.tela_cadastro.tableWidget.rowCount() + 1)
-        self.tela_cadastro.tableWidget.setItem(self.tela_cadastro.tableWidget.rowCount() - 1, 0, QTableWidgetItem(nome_produto))
-        self.tela_cadastro.tableWidget.setItem(self.tela_cadastro.tableWidget.rowCount() - 1, 1, QTableWidgetItem(valor_produto))
-        
-        self.tela_compra.tableWidget.setRowCount(self.tela_compra.tableWidget.rowCount() + 1)
-        self.tela_compra.tableWidget.setItem(self.tela_compra.tableWidget.rowCount() - 1, 0, QTableWidgetItem(nome_produto))
-        self.tela_compra.tableWidget.setItem(self.tela_compra.tableWidget.rowCount() - 1, 1, QTableWidgetItem(valor_produto))
-        
-        self.tela_cadastro.lineEdit.clear()
-        self.tela_cadastro.lineEdit_2.clear()
-    
-        
-        
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = tela_inicial()
-    window.show()
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
     sys.exit(app.exec_())
